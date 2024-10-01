733b6c409e6ad66b42e1032f2699b29533469af4
Shivay @ Couchbase <162308263+shivay-couchbase@users.noreply.github.com>
2024-10-01 08:45:47 +0530

    Update DatabaseManager.kt

--- app/src/main/java/com/example/quizappbycouchbase/DatabaseManager.kt
+++ app/src/main/java/com/example/quizappbycouchbase/DatabaseManager.kt
@@ -220,7 +220,7 @@ class DatabaseManager(private val context: Context) {
                 imagesCategoryCollection?.save(mutableDocument)
             }
             Log.i("JSON", "ALL JSON ADDED")
-            val config = VectorIndexConfiguration("imagevect_l2", 512, 3)
+            val config = VectorIndexConfiguration("imagevect_l2", 1408 , 3)
             config.encoding = VectorEncoding.none()
             imagesCategoryCollection!!.createIndex(INDEX_NAME, config)
 
